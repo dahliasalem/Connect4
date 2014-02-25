3f9dac66fa0e1fb52a1ccc1a5ed903d6af922924
Ben Roye <benjamin.reed.roye@gmail.com>
2014-02-24 20:50:41 -0800

    Evaluates vertical threats only.

--- minimax_zugzwang.java
+++ minimax_zugzwang.java
@@ -13,10 +13,12 @@ import java.awt.Point;
  * @see RandomAI, StupidAI, MonteCarloAI
  */
 
-public class minimax_connectTHIS extends AIModule
+public class minimax_zugzwang extends AIModule
 {
 	private int[] moves;
 
+	private int myID;
+	private int eID;
 	private int depth;
 	private int width;
 	private int height;
@@ -27,16 +29,19 @@ public class minimax_connectTHIS extends AIModule
 	private static final int NOLOSS = 1000;
 	private static final int NEWTHREAT = 100;
 
-	public minimax_connectTHIS() {
+	public minimax_zugzwang() {
+		myID = eID = 0;
 		depth = 0;
 		width = 7;
 		height = 6;
-		threats = new int[width][height];
+		myThreats = new int[width][height];
+		eThreats = new int[width][height];
 		heights = new int[width];
 
 		for(int i = 0; i < width; i++) {
 			for(int j = 0; j < height; j++) {
-				threats[i][j] = 0;
+				myThreats[i][j] = 0;
+				eThreats[i][j] = 0;
 			}
 		}
 		for(int i = 0; i < width; i++) {
@@ -45,6 +50,10 @@ public class minimax_connectTHIS extends AIModule
 	}
 
 	public void getNextMove(final GameStateModule state){
+		if(!myID) {
+			myID = state.getActivePlayer();
+			eID = myID == 1 ? 2 : 1;
+		}
 		//only uncomment these if we have reason to believe there will be non-standard sizes
 		//width = state.getWidth();
 		//height = state.getHeight();
@@ -65,7 +74,15 @@ public class minimax_connectTHIS extends AIModule
 			}
 		}
 		while(!terminate) {
-			
+			if(h > 2) {
+				// has a vertical threat been created?
+				if(state.getAt(enemyMove, h-1) == eID && state.getAt(enemyMove, h-2) == eID) {
+					eThreats[enemyMove][h+1] = eID;
+				}
+				if(enemyMove > 0 && enemyMove < 5 && state.getAt(enemyMove+1, h-1) == eID && state.getAt(enemyMove+2, h-2)) {
+					eThreats[enemyMove-1][h+1] = eID;
+				}
+			}
 		}
 		heights[chosenMove]++;
 		depth = 0;
